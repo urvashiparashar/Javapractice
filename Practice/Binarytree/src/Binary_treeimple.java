25dcb3cc3af23e029bf3ba715a52c2032014e99e
urvashiparashar <urvashiparashar2001@gmail.com>
2022-10-16 00:18:12 +0530

    added file via upload

--- Practice/Binarytree/src/Binary_treeimple.java
+++ Practice/Binarytree/src/Binary_treeimple.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 //binary tree has max 2 childrens
 //root is called node and subnodes are called left and right child
 //leaf node: nodes with no child
@@ -6,8 +9,8 @@ public class Binary_treeimple {
 	static class Node
 	{
 		int data;
-	    static Node left;
-	    static Node right;
+	    Node left;
+	    Node right;
 	    
 	    Node(int data)
 	    {
@@ -33,6 +36,105 @@ public class Binary_treeimple {
 			return newnode;
 		}
 	}
+	
+	public static void preorder(Node root)
+	{
+		//1. root
+		//2. left subtree
+		//3. right subtree
+		if(root==null)
+		{
+			System.out.print(-1+" ");
+			return ; //return from that particular point
+		}
+		//recursion
+		System.out.print(root.data+" ");
+		preorder(root.left);
+		preorder(root.right);
+	}
+	
+	public static void inorder(Node root)
+	{
+		
+		//1. left subtree
+		//2. root
+		//3. right subtree
+		if(root==null)
+		{
+			System.out.print(-1+" ");
+			return ; //return from that particular point
+		}
+		//recursion
+		
+		inorder(root.left);
+		System.out.print(root.data+" ");
+		inorder(root.right);
+	}
+	
+	public static void postorder(Node root)
+	{
+		
+		//1. left subtree
+		//2. right subtree
+		//3. root
+		
+		if(root==null)
+		{
+			System.out.print(-1+" ");
+			return ; //return from that particular point
+		}
+		//recursion
+		
+		postorder(root.left);
+		
+		postorder(root.right);
+		System.out.print(root.data+" ");
+	}
+	
+	public static void levelorder(Node root)
+	{
+		//base case
+		if(root==null)
+		{
+			return;
+		}
+		Queue<Node>q=new LinkedList<>();
+		q.add(root);
+		q.add(null);
+		
+		while(!q.isEmpty())
+		{
+			Node currnode=q.remove(); //remove this node
+			if(currnode==null)
+			{
+				System.out.println();
+				if(q.isEmpty())
+				{
+					break;
+				}
+				else
+				{
+					q.add(null);
+				}
+			}
+			else
+			{
+				System.out.print(currnode.data+" ");
+				if(currnode.left!=null)
+				{
+					q.add(currnode.left);
+				}
+				if(currnode.right!=null)
+				{
+					q.add(currnode.right);
+				}
+			}
+		}
+		
+		
+		
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -40,7 +142,11 @@ public class Binary_treeimple {
 		int nodes[]= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
 		Binarytree bt=new Binarytree();
 		Node root=bt.buildtree(nodes);
-		System.out.println(root.data);
+//		System.out.println(root.data);
+//		preorder(root);
+//		inorder(root);
+//		postorder(root);
+		levelorder(root);
 
 	}
 
