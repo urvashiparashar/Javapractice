5244f804c2007e85be4e5d4946d3712c84a7e4e3
urvashiparashar <urvashiparashar2001@gmail.com>
2022-11-04 11:01:25 +0530

    added via upload

---
+++ Practice/Questions_cwh/src/Questions_onconditionals.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Questions_onconditionals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Question2
+//		byte m1,m2,m3; //byte as we take marks out of 100
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("enter marks in physics");
+//		m1=sc.nextByte();
+//		System.out.println("enter marks in chemistry");
+//		m2=sc.nextByte();
+//		System.out.println("enter marks in maths");
+//		m3=sc.nextByte();
+//		//avg
+//		float avg=(m1+m2+m3)/3.0f; //int dividedd by float
+//		System.out.println("your overall percentage is "+avg);
+//		if(avg>=40 && m1>=33 && m2>=33 && m3>=33)
+//		{
+//			System.out.println("congratulations you have been promoted");
+//		}
+//		else
+//		{
+//			System.out.println("sorry you are not promoted");
+//		}
+//		
+		
+		//Question 3
+//		float tax=0;
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("enter your income in lakhs per annum");
+//		float income=sc.nextFloat();
+//		if(income<2.5)
+//		{
+//			tax=tax+0; //no need to pay tax if less than 2.5
+//		}
+//		else if(income>2.5f && income<=5f)
+//		{
+//			tax=tax+0.05f*(income-2.5f);
+//			
+//		}
+//		else if(income>5f && income<=10.0f)
+//		{
+//			tax=tax+0.05f*(5.0f-2.5f);
+//			tax=tax+0.2f*(income-5f);
+//		}
+//		else if(income>10.0f)
+//		{
+//			tax=tax+0.05f*(5.0f-2.5f);
+//			tax=tax+0.2f*(10.0f-5.0f);
+//			tax=tax+0.3f*(income-10.0f);
+//		}
+//		System.out.println("total tax paid by employee is "+tax);
+		
+		
+		//Question 4
+		Scanner sc=new Scanner(System.in);
+		int year=sc.nextInt();
+		Boolean leap=false;
+		//leap year (except centuries) if divisible by 4 and for centuries if divisible by 400
+		if(year%4==0)
+		{
+			if(year%100==0)
+			{
+				
+			}
+		}
+		
+
+	}
+
+}
